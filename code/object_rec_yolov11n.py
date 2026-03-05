2955ebb68e821d0f2d685e11195a3740cad3fd26
Michelle Ong <michelleongshutian@gmail.com>
2026-03-05 20:09:41 +0800

    add ugot version of object detection

--- code/object_rec_yolov11n.py
+++ code/object_rec_yolov11n.py
@@ -1,14 +1,30 @@
 import cv2
 from ultralytics import YOLO
 
-model = YOLO("best.pt")
+model = YOLO("yolo11n.pt")
 
 cap = cv2.VideoCapture(0)
 
+## UGOT version
+# import numpy as np
+# from ugot import ugot
+# got = ugot.UGOT()
+# got.initialize("192.168.1.189") # replace this with your UGOT IP address
+# got.open_camera()
+
 while True:
+    # webcam
     ret, frame = cap.read()
     if not ret:
         break
+    # ------------------------------
+    ## UGOT version
+    # frame = got.read_camera_data()
+    # if frame is None:
+    #     continue
+    # nparr = np.frombuffer(frame, np.uint8)
+    # frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    # ------------------------------
 
     results = model(frame, conf=0.4, imgsz=640, device="cpu")
 
@@ -22,7 +38,7 @@ while True:
             cv2.putText(frame, f"{model.names[cls]} {conf:.2f}",
                         (x1, y1 - 8), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
 
-    cv2.imshow("YOLOv8", frame)
+    cv2.imshow("YOLOv11", frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
